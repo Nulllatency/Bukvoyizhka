998510bd641f90b6bacb4115736615d74d625ed8
LetsBuildSomeSh1t <ltsbldsmsht>
2023-06-06 23:54:29 +0300

    change core 6 to 7

--- Bukvoyizhka.CLI/Program.cs
+++ Bukvoyizhka.CLI/Program.cs
@@ -12,11 +12,12 @@ string helloMessage = """"
 
     -sf=N - start from number of recipient 
     -mc=N - mutation chance 0-10
+
     recipient = address where mail was sent
     RECIPIENTS = path to file
     ---------------------------
     Commands:
-        test*DONT WORKIN* <ACCS> <recipient>  <MAIL> <subject>
+        test *doesn't work* <ACCS> <recipient>  <MAIL> <subject>
         send <ACCS> <RECIPIENTS> <MAIL> <subject>
         q - quit program
 
